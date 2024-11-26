699f441985e01e20492cc9186cc0f785eb602b9e
tbsl1730 <david.george@orange.com>
2024-11-26 12:39:34 +0100

    fixed date time format

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    today = datetime.date.now()
-    return "Hello, World! " + today
+    today = datetime.datetime.now()
+    return "Hello, World! " + str(today)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
