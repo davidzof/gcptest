8b6b9a320f560150654bda7a8a441ff043f5553c
David George <david.george@gmail.com>
2024-11-26 12:33:42 +0100

    Update app.py
    print date and time

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 # app.py
 from flask import Flask
+import datetime
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def hello_world():
-    return "Hello, World! " + __name__
+    today = datetime.date.now()
+    return "Hello, World! " + today
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
